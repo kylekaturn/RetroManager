7c7da5906f11db605d4380ae1c52487eb9b94488
kylekaturn <kylekaturn@gmail.com>
2025-05-12 00:05:19 +0900

    초기 커밋

---
+++ RetroManager/RetroManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetroManagerApp.swift
+//  RetroManager
+//
+//  Created by Daewoo Kim on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RetroManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
