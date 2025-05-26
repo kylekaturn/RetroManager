ac1ecc9446335a38319e709861410fac63cafc42
kylekaturn <kylekaturn@gmail.com>
2025-05-26 13:14:41 +0900

    Seperate game class from playlist class

---
+++ RetroManager/Models/Game.swift
@@ -0,0 +1,50 @@
+import Foundation
+import AppKit
+
+class Game: Codable, Identifiable, Hashable{
+    var id : UUID = UUID()
+    var path: String
+    var label: String
+    var core_path: String
+    var core_name: String
+    var crc32: String
+    var db_name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case path
+        case label
+        case core_path
+        case core_name
+        case crc32
+        case db_name
+    }
+    
+    init(){
+        path = "Path"
+        label = "Label"
+        core_path = "CorePath"
+        core_name = "CoreName"
+        crc32 = "CRC32"
+        db_name = "DBName"
+    }
+    
+    static func == (lhs: Game, rhs: Game) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    //json 스트링을 변환
+    func toJson() -> String {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = [.prettyPrinted, .withoutEscapingSlashes]
+        if let data = try? encoder.encode(self),
+           let jsonString = String(data: data, encoding: .utf8) {
+           return jsonString
+        }else{
+            return ""
+        }
+    }
+}
